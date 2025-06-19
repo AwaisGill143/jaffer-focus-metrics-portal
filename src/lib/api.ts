d65c6138ca5f688461b17092d4898d16af938db5
AwaisGill143 <159516776+AwaisGill143@users.noreply.github.com>
2025-06-19 16:40:00 +0500

    changed url for deployment

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { generateFallbackGoals } from './fallback';
 import { OKRData } from '@/types';
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
+
 
 
 /**
